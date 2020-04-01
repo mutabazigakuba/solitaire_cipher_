10ecd4ef2a6d642c2d20bcaa89422fdfe6dc0032
mutabazigakuba <gakubar2@gmail.com>
2020-04-01 14:14:29 +0200

    tested that the encode function retrurns only a string after removing non-string values

--- solitaire/src/main/java/solitaire/Solitaire.java
+++ solitaire/src/main/java/solitaire/Solitaire.java
@@ -1,9 +1,20 @@
 package solitaire;
 
+
 public class Solitaire{
 
     public String encoded_message(String message){
+
         String make_upper_case = message.toUpperCase();
-        return make_upper_case;
+
+        String[] string_values = make_upper_case.split("\\W+");
+
+        String new_message = new String();
+
+        for (int i = 0; i < string_values.length; i++) {
+            new_message = new_message + string_values[i];
+        }
+
+        return new_message;
     }
 }
--- solitaire/src/test/java/solitaire/SolitaireTest.java
+++ solitaire/src/test/java/solitaire/SolitaireTest.java
@@ -11,13 +11,19 @@ public class SolitaireTest{
     @Test
     public void input_should_only_be_a_string(){
 
-        assertEquals("Hello", solitaire.encoded_message("Hello"));
+        assertEquals("HELLO", solitaire.encoded_message("Hello"));
     }
 
     @Test
     public void input_should_converted_to_upper_case(){
 
         assertEquals("HELLO", solitaire.encoded_message("hello"));
+    }
+
+    @Test
+    public void input_should_be_converted_letters_only(){
+
+        assertEquals("HELLO", solitaire.encoded_message("@he%llo!"));
     }
     
 }
