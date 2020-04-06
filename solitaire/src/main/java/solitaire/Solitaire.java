168ddfc360f0a6bb71e8c68ed1d18c467b8879ed
Mihigo Rugamba <robert.rugamba3@hotmail.com>
2020-04-06 07:15:39 +0200

    refactor to add message property and constructor initialization

--- solitaire/src/main/java/solitaire/Solitaire.java
+++ solitaire/src/main/java/solitaire/Solitaire.java
@@ -2,42 +2,48 @@ package solitaire;
 
 import java.util.Arrays;
 
-public class Solitaire {
-
-    public String encoded_message(String message) 
+public class Solitaire 
+{
+    private String _message;
+    public Solitaire(String message)
     {
-        String cleanedMessage = removeNonLetters(message);
-        String changeToUpper = changeToUpperCase(cleanedMessage);
-        String groupedIntoFives = groupLettersIntoFives(changeToUpper);
-        String finalGroupedMessage = AddXs(groupedIntoFives);
-        return finalGroupedMessage;
+        _message = message;
     }
 
-    private String changeToUpperCase(String message) {
-        return message.toUpperCase();
-    }
-
-    private String removeNonLetters(String message) {
-        return message.replaceAll("[^\\p{L}\\p{Z}]","");
-    }
-
-    private String groupLettersIntoFives(String message) 
+    public String encoded_message() 
     {
-        return Arrays.toString(message.replaceAll("\\s+", "").split("(?<=\\G.{" + 5 + "})"))
+        _message = removeNonLetters();
+        _message = changeToUpperCase();
+        _message = groupLettersIntoFives();
+        _message = AddXs();
+        return _message;
+    }
+
+    private String removeNonLetters() 
+    {
+        return _message.replaceAll("[^\\p{L}\\p{Z}]","");
+    }
+
+    private String changeToUpperCase() 
+    {
+        return _message.toUpperCase();
+    }
+
+    private String groupLettersIntoFives() 
+    {
+        return Arrays.toString(_message.replaceAll("\\s+", "").split("(?<=\\G.{" + 5 + "})"))
             .replaceAll("[^\\p{L}\\p{Z}]","");
     }
 
-    private String AddXs (String message)
+    private String AddXs()
     {
-        String removeSpace = message.replaceAll("\\s+", "");
+        String removeSpace = _message.replaceAll("\\s+", "");
         int numberXtoAdd = removeSpace.length() % 5;
 
-        for (int i = 1; i < numberXtoAdd; i++) {
-            message = message + "X";
+        for (int i = 1; i < numberXtoAdd; i++) 
+        {
+            _message = _message + "X";
         }
-        System.out.println(message);
-
-        return message;
+        return _message;
     }
-
 }
--- solitaire/src/test/java/solitaire/SolitaireTest.java
+++ solitaire/src/test/java/solitaire/SolitaireTest.java
@@ -10,10 +10,10 @@ public class SolitaireTest
     @Test
     public void message_should_be_converted_into_upper_case()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hello");
 
         String expectedMessage = "HELLO";
-        String actualMessage = solitaire.encoded_message("hello"); 
+        String actualMessage = solitaire.encoded_message(); 
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -21,10 +21,10 @@ public class SolitaireTest
     @Test
     public void message_with_special_chars_should_be_converted_into_letters_only()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hel,#$lo%");
 
         String expectedMessage = "HELLO";
-        String actualMessage = solitaire.encoded_message("hel,#$lo%");
+        String actualMessage = solitaire.encoded_message();
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -33,10 +33,10 @@ public class SolitaireTest
     @Test
     public void message_with_numbers_should_be_converted_into_letters_only()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hel37lo5");
 
         String expectedMessage = "HELLO";
-        String actualMessage = solitaire.encoded_message("hel37lo5");
+        String actualMessage = solitaire.encoded_message();
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -44,10 +44,10 @@ public class SolitaireTest
     @Test
     public void message_without_space_should_be_grouped_into_fives()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hellohello");
 
         String expectedMessage = "HELLO HELLO";
-        String actualMessage = solitaire.encoded_message("hellohello"); 
+        String actualMessage = solitaire.encoded_message(); 
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -55,10 +55,10 @@ public class SolitaireTest
     @Test
     public void message_with_space_should_be_grouped_into_fives()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hello hel lo");
 
         String expectedMessage = "HELLO HELLO";
-        String actualMessage = solitaire.encoded_message("hello hel lo"); 
+        String actualMessage = solitaire.encoded_message(); 
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -66,10 +66,10 @@ public class SolitaireTest
     @Test
     public void message_without_space_should_be_grouped_in_fives_and_x_added_when_not_modulus_five()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hellohel");
 
         String expectedMessage = "HELLO HELXX";
-        String actualMessage = solitaire.encoded_message("hellohel"); 
+        String actualMessage = solitaire.encoded_message(); 
 
         assertEquals(expectedMessage, actualMessage);
     }
@@ -78,10 +78,10 @@ public class SolitaireTest
     @Test
     public void message_with_space_should_be_grouped_in_fives_and_x_added_when_not_modulus_five()
     {
-        Solitaire solitaire = new Solitaire();
+        Solitaire solitaire = new Solitaire("hel loh el");
 
         String expectedMessage = "HELLO HELXX";
-        String actualMessage = solitaire.encoded_message("hel loh el"); 
+        String actualMessage = solitaire.encoded_message(); 
 
         assertEquals(expectedMessage, actualMessage);
     }
