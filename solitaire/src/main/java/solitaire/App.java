4f5465416a95b8cd2c54087206579a880af6d15f
mutabazigakuba <gakubar2@gmail.com>
2020-05-06 21:33:30 +0200

    finished with the validate message class and tests

--- solitaire/src/main/java/solitaire/App.java
+++ solitaire/src/main/java/solitaire/App.java
@@ -1,10 +1,47 @@
 package solitaire;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main(String[] args) 
     {
-        Commandline cmd = new Commandline();
-        cmd.mRunConsole();
+        int numberOfTimes = 1;
+        for (int i = 0; i < numberOfTimes; i++) 
+        {
+            System.out.println("======================");
+            System.out.println("Enter message");
+            Scanner scanner = new Scanner(System.in);
+
+            Validate validate = new Validate(scanner.nextLine());
+            validate.mValidateMessage();
+            String validatedMessage = validate.validatedMessage;
+
+            Commandline cmd = new Commandline(validatedMessage);
+            cmd.mRunConsole();
+
+            System.out.println("======================");
+            System.out.println("Add another message? ");
+            System.out.println("Type Y/N ");
+            Scanner scanner2 = new Scanner(System.in);
+
+            switch (scanner2.nextLine()) 
+            {
+                case "Y":
+                    numberOfTimes += 1;
+                    break;
+
+                case "N":
+                    numberOfTimes -= 1;
+                    scanner.close();
+                    break;
+
+                default:
+                    System.out.println("Unknown Input! ");
+                    scanner2.close();
+                    scanner.close();
+            }
+        }
+
     }
 }
--- solitaire/src/main/java/solitaire/Commandline.java
+++ solitaire/src/main/java/solitaire/Commandline.java
@@ -2,89 +2,43 @@ package solitaire;
 
 import java.util.Scanner;
 
-public class Commandline 
-{
+public class Commandline {
 
     Scanner scanner = new Scanner(System.in);
     public String userInput = "";
     private int[] deck = new int[54];
-    private int numberOfTimes = 1;
 
-    public void mRunConsole()
-    {
-        for (int i = 0; i < numberOfTimes; i++) {
-            mGetUserInput();
-            generateDeckOfCards();
-            mDisplayEncryptInput();
-            generateDeckOfCards();
-            mDisplayDecryptInput();
-            mContinueCipher();
-        }
+    public Commandline(String validateMessage) {
+        userInput = validateMessage;
     }
 
-    public void mGetUserInput()
+    public void mRunConsole() 
     {
-        System.out.println("======================");
-        System.out.println("Enter message");
-        userInput = scanner.nextLine();
-        if(userInput.isEmpty())
-        {
-            System.out.println("Enter a valid message please!");
-        }
-        else
-        {
-            System.out.println("Your message is "+userInput);
-        }
+        generateDeckOfCards();
+        mDisplayEncryptInput();
+        generateDeckOfCards();
+        mDisplayDecryptInput();
     }
 
-    private void mDisplayEncryptInput()
-    {
+    private void mDisplayEncryptInput() {
         userInput = mIntializeSolitaire().mEncryptMessage();
-        System.out.println("Encrypted message is "+userInput);
+        System.out.println("Encrypted message is " + userInput);
     }
 
-    private void mDisplayDecryptInput()
-    {
+    private void mDisplayDecryptInput() {
         userInput = mIntializeSolitaire().mDecryptMessage();
-        System.out.println("Decrpyted message is "+userInput);
+        System.out.println("Decrpyted message is " + userInput);
     }
 
-    private void mContinueCipher()
-    {
-        System.out.println("======================");
-        System.out.println("Add another message? ");
-        System.out.println("Type Y/N ");
-        Scanner scanner = new Scanner(System.in);
-        String answer = scanner.nextLine();
-        
-        switch(answer){
-            case "Y":
-                numberOfTimes += 1;
-                break;
-            
-            case "N":
-                numberOfTimes -= 1;
-                scanner.close();
-                break;
-            
-            default: 
-                System.out.println("Unknown Input! ");
-                scanner.close();
-        }
-    }
-
-    private void generateDeckOfCards()
-    {
+    private void generateDeckOfCards() {
         int j = 54;
-        for (int i = 0; i < deck.length; i++) 
-        {
+        for (int i = 0; i < deck.length; i++) {
             deck[i] = j;
             j--;
         }
     }
 
-    private Solitaire mIntializeSolitaire()
-    {
+    private Solitaire mIntializeSolitaire() {
         Key generatedkKey = new Key(deck);
         Message message = new Message(userInput);
         Solitaire solitaire = new Solitaire(generatedkKey, message);
