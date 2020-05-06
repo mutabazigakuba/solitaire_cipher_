b8333ff2ff7bc8d612397218ce59fabc3a37fe13
mutabazigakuba <gakubar2@gmail.com>
2020-05-06 21:42:19 +0200

    fixing circleCI errors in App class

---
+++ solitaire/src/main/java/solitaire/Validate.java
@@ -0,0 +1,56 @@
+package solitaire;
+
+import java.util.Scanner;
+
+public class Validate 
+{
+    public String errorMessage = "";
+    private String InputMessage = "";
+    public String validatedMessage = "";
+    Scanner scanner = new Scanner(System.in);
+
+    public Validate(String message)
+    {
+        InputMessage = message;
+    }
+
+    public void mValidateMessage()
+    {
+        mRemoveSpace();
+        mRemoveNumbers();
+        mRemoveSpecialChars();
+        validatedMessage = InputMessage;
+    }
+
+    public void mRemoveSpace()
+    {
+        String newInputString = InputMessage.replaceAll("\\s+", "");
+        InputMessage = newInputString;
+        mStringIsEmpty("Empty String Not Accepted");
+    }
+
+    public void mRemoveSpecialChars()
+    {
+        String newInputString = InputMessage.replaceAll("[^a-zA-Z0-9]", "");
+        InputMessage = newInputString;
+        mStringIsEmpty("Should Not Be Special Chararacters Only");
+    }
+
+    public void mRemoveNumbers()
+    {
+        String newInputString = InputMessage.replaceAll("[^a-zA-Z]", "");
+        InputMessage = newInputString;
+        mStringIsEmpty("Should Not Be Numbers Only");
+    }
+
+    private void mStringIsEmpty(String message)
+    {
+        if(InputMessage.isEmpty()){
+            errorMessage = message;
+            System.out.println(errorMessage);
+        }
+        else{
+            errorMessage = "Accepted";
+        }
+    }
+}
---
+++ solitaire/src/test/java/solitaire/ValidateTest.java
@@ -0,0 +1,100 @@
+package solitaire;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ValidateTest 
+{
+    
+    @Test
+    public void shouldNotAcceptEmptyString()
+    {
+        String message = "";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Empty String Not Accepted";
+        validate.mRemoveSpace();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldNotAcceptSpacesOnly()
+    {
+        String message = "  ";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Empty String Not Accepted";
+        validate.mRemoveSpace();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldNotAcceptMessageWithSpecialCharatersOnly()
+    {
+        String message = "#@$%&^";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Should Not Be Special Chararacters Only";
+        validate.mRemoveSpecialChars();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldNotAcceptNumbersOnly()
+    {
+        String message = "12345";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Should Not Be Numbers Only";
+        validate.mRemoveNumbers();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldAcceptStringWithSpecialChars()
+    {
+        String message = "hello#$%^";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Accepted";
+        validate.mRemoveSpecialChars();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldAcceptStringWithNumbers()
+    {
+        String message = "hello1234";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Accepted";
+        validate.mRemoveNumbers();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    public void shouldAcceptStringWithCharsOnly()
+    {
+        String message = "hello";
+        Validate validate = new Validate(message);
+
+        String expectedErrorMessage = "Accepted";
+        validate.mValidateMessage();
+        String actualErrorMessage = validate.errorMessage;
+
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+}
